45d90d626ebe927f851f3253d2a0df94d6c2e0aa
Lisbeth Lepp <LisbethLepp12@gmail.com>
2025-11-17 11:27:24 +0200

    Updating store.js

--- script/store.js
+++ script/store.js
@@ -3,18 +3,29 @@ import { createStore } from 'vuex'
 
 // Creating a new store instance
 const store = createStore({
+
+    //This is where the app's shared data is
+    //state is like a global JSON object - everyone can use it
     state () {
         return {
-            count:0
+            posts: [{},{},{},{},{},{},{},{},{},{}]
         }
     },
+
+    //These are functions :)
     mutations: {
-        increment (state) {
-            state.count++
+        incrementLikes(state, postId) {
+            const post = state.posts.find(p => p.id === postId)
+            if (post) post.likes++
+        },
+
+        resetLikes(state) {
+            state.posts.forEach(p => p.likes = 0)
         }
     }
 })
 
 const app = createApp({})
 
+// Installing the store instance as a plugin
 app.use(store)
